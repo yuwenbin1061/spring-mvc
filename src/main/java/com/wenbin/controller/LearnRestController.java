f51357c4777b26c29fbec13375ab1c8d8a858d5c
wenbin.yu <wenbin.yu@china.nttdata.com>
2024-07-26 16:49:49 +0800

    初始化springmvc

--- src/main/java/com/wenbin/controller/LearnRestController.java
+++ src/main/java/com/wenbin/controller/LearnRestController.java
@@ -2,7 +2,6 @@ package com.wenbin.controller;
 
 import com.wenbin.pojo.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
